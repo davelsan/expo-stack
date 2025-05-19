0d3b140fcae9d94256d255d5d00ebf4943c063ad
David Velasco <davvelsan@gmail.com>
2025-05-19 11:39:35 +0200

    clean monorepo setup

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,6 @@
 
 // Learn more https://docs.expo.dev/guides/monorepos
 // Learn more https://docs.expo.io/guides/customizing-metro
-const path = require('path');
 
 const { getDefaultConfig } = require('expo/metro-config');
 const {
@@ -11,23 +10,17 @@ const {
 
 // Find the project and workspace directories
 const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(projectRoot, {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
 });
 
-// Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
-
-config.resolver = {
-  ...config.resolver,
-  // Let Metro know where to resolve packages and in what order
-  nodeModulesPaths: [
-    path.resolve(projectRoot, 'node_modules'),
-    path.resolve(workspaceRoot, 'node_modules'),
-  ],
-};
+/**
+ * Opt-out of the new ES Modules resolution.
+ * @see https://docs.expo.dev/versions/latest/config/metro/#es-module-resolution
+ */
+// config.resolver.unstable_enablePackageExports = false;
 
 module.exports = wrapWithReanimatedMetroConfig(config);
