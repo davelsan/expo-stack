9de18f26f910cfd78698f9366d2953dffc92a3ab
David Velasco <davvelsan@gmail.com>
2025-06-10 20:29:07 +0200

    tests: fix matching supporting non-test files

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,16 @@ const config: Config = {
     'react-native-unistyles/mocks',
     './src/theme/unistyles.ts',
   ],
+  /**
+   * Only match `*.test.tsx?` or `*.spec.tsx?` files.
+   * This ignores supporting files, such as `utils.ts`.
+   * @see https://jestjs.io/docs/configuration#testmatch-arraystring
+   */
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+    '**/__tests__/**/*.spec.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
   transformIgnorePatterns: [
     'node_modules/(?!(?:.pnpm/)?((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg))',
   ],
